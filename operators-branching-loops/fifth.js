994ac79517fea774ebd531c44e253dcb610b7d1d
tushargoyal812 <tushargoyal812@gmail.com>
2021-09-14 20:38:56 +0530

    fifth and sixth done

---
+++ operators-branching-loops/fifth.js
@@ -0,0 +1,14 @@
+var n1=35,n2=29,n3=46;
+
+if(n1<n2&&n1<n3)
+{
+    console.log(n1+' is lowest');
+}
+if(n2<n1&&n2<n3)
+{
+    console.log(n2+' is lowest');
+}
+if(n3<n2&&n3<n1)
+{
+    console.log(n3+' is lowest');
+}
