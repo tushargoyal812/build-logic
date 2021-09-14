e7e85b513e5a9d269b2a71172bd81185ba6d5ce7
tushargoyal812 <tushargoyal812@gmail.com>
2021-09-14 20:32:06 +0530

    third and fourth done

---
+++ operators-branching-loops/fourth.js
@@ -0,0 +1,14 @@
+var num1=8,num2=23,num3=17;
+
+if(num1>num2&&num1>num3)
+{
+    console.log(num1+' is grater');
+}
+if(num2>num1&&num2>num3)
+{
+    console.log(num2+' is grater');
+}
+if(num3>num1&&num3>num2)
+{
+    console.log(num3+' is grater');
+}
---
+++ operators-branching-loops/third.js
@@ -0,0 +1,6 @@
+var num1=129;
+var num2=251;
+if(num1<num2)
+{
+    console.log(num1+' is minimum and '+num2+' is maximum');
+}
