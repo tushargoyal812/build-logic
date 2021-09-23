b91f676699795453e067190a196765f1416d6ab7
tushargoyal812 <tushargoyal812@gmail.com>
2021-09-23 12:20:35 +0530

    easy one and two done

---
+++ functions/e-1.js
@@ -0,0 +1,12 @@
+// function power(a,b)
+// {
+//     var d=1
+//     for(let i=1;i<=b;i++)
+//     {
+//         d=d*a
+//     }
+//     console.log(d)
+// }
+
+var a=Math.pow(5,3)
+console.log(a)
---
+++ functions/e-2.js
@@ -0,0 +1,8 @@
+function hexagon(a)
+{
+    return 3*Math.sqrt(3)*a*a/2
+}
+
+var h=hexagon(20)
+
+console.log(h.toFixed(2))
