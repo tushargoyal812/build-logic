6d456f3d11836c975d1703c5bc58a0a105d14b4d
tushargoyal812 <tushargoyal812@gmail.com>
2021-09-26 09:14:22 +0530

    minimun number with rest parametres

--- functions/e-4.js
+++ functions/e-4.js
@@ -1,15 +1,13 @@
-function findMin(a, b, c, d) {
-    if (a < b && a < c && a < d) {
-        return a
-    } else if (b < a && b < c && b < d) {
-        return b
-    } else if (c < a && c < b && c < d) {
-        return c;
-    } else if (d < a && d < b && d < c) {
-        return d
+function findMin(...numbers) {
+    var min=numbers[0];
+    console.log(numbers.length)
+    for(let i=0;i<numbers.length;i++)
+    {
+        if(numbers[i]<min)
+        {
+            min=numbers[i]
+        }
     }
+    console.log(min)
 }
-
-var min = findMin(14,2,25,3)
-
-console.log(min)
+findMin(14,2,25,3)
