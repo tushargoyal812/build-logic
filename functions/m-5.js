a6d1fb7ece2bb1bd1a9d32312920c5ddd9fecb64
tushargoyal812 <tushargoyal812@gmail.com>
2021-09-28 09:49:15 +0530

    charAt done

---
+++ functions/m-5.js
@@ -0,0 +1,13 @@
+
+function charAt(str,index)
+{
+    for(let i=0;i<str.length;i++)
+    {
+        if(i===index)
+        {
+            console.log(str[i])
+        }
+    }
+}
+
+charAt("tushar",2)
