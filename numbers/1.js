e4f665c1091dc3419955fabe80b0ebdf3831a481
tushargoyal812 <tushargoyal812@gmail.com>
2021-09-29 19:26:54 +0530

    1 done

---
+++ numbers/1.js
@@ -0,0 +1,8 @@
+var readlineSync=require('readline-sync');
+
+var input1=readlineSync.question('input1: ')
+var input2=readlineSync.question('input2: ')
+
+var sum=Number(input1)+Number(input2);
+
+console.log(sum)
